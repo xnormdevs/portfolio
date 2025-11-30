12aed6b21299fa57474e147a59d4ef4b737483b0
thamara1997 <darshanathamara143@gmail.com>
2025-11-30 08:57:51 +0530

    github deployment yaml

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,12 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
+    output: "export",
+    images: { unoptimized: true },
+    basePath: "/portfolio",             // <-- replace "portfolio" with your repo name
+    assetPrefix: "/portfolio/",         // <-- same here
+    pageExtensions: ["js","jsx","ts","tsx","md","mdx"],
+    experimental: { mdxRs: true },
 };
 
 export default withContentlayer(nextConfig);
